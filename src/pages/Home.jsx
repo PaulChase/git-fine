37489d1a6f693227a88fd6156142c30e2c4a9dc1
Ajonye Paul <ajonyechase@gmail.com>
2022-12-19 00:08:22 +0100

    change site name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,12 +26,15 @@ export default function Home() {
 			<div className=" bg-black/90 absolute top-0 left-0 w-full h-full"></div>
 			<div className=" relative z-10 h-full w-full flex justify-center items-center text-center flex-col space-y-10 px-4">
 				<h1 className=" text-5xl md:text-6xl font-extrabold ">
-					<span className=" fa fa-user-circle"></span> Git-Fine
+					<span className=" fa fa-user-circle"></span> Git-Folio
 				</h1>
 
 				<p className=" font-semibold text-xl text-gray-50">Generate beautiful portfolios from your Github profile</p>
 
-				<form className=" w-full lg:w-[45rem]  mx-auto flex items-center" onSubmit={handleSubmit}>
+				<form
+					className=" w-full lg:w-[45rem]  mx-auto flex items-center flex-col md:flex-row space-y-3 md:space-y-0"
+					onSubmit={handleSubmit}
+				>
 					<input
 						type="text"
 						className="outline-none w-full px-4 py-3  text-gray-600"
@@ -40,9 +43,21 @@ export default function Home() {
 						onChange={handleChange}
 					/>
 					<button className="bg-fuchsia-600 px-10 py-3  font-bold text-white flex items-center group">
-						Generate <span className=" fa fa-chevron-right ml-2 group-hover:ml-4 transition-all ease-linear"></span>
+						Generate
+						<span className=" fa fa-chevron-right ml-2 group-hover:ml-4 transition-all ease-linear"></span>
 					</button>
 				</form>
+
+				<div className=" font-semibold">
+					<p>Designed & developed by:</p>
+					<a
+						href="https://github.com/paulchase"
+						target={"_blank"}
+						className=" block font-bold px-4 py-2 border-gray-200 border-2 mt-2"
+					>
+						<span className=" fa fa-github mr-2"></span> Ajonye Paul
+					</a>
+				</div>
 			</div>
 		</main>
 	);
