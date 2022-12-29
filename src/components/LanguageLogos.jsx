16a12efad3b2cec8a0e17d2eab6d242920fa698a
Ajonye Paul <ajonyechase@gmail.com>
2022-12-29 21:45:19 +0100

    fix deloy error

--- src/components/LanguageLogos.jsx
+++ src/components/LanguageLogos.jsx
@@ -5,11 +5,21 @@ export default function LanguageLogos({ languages }) {
 	return (
 		<div className=" space-x-3 flex items-center whitespace-nowrap overflow-x-auto py-4 scrollbar-none ">
 			{Array.from(new Set(languages)).map((language, index) => (
-				<Image
-					key={index}
-					src={`/logos/${language}.svg`}
-					className=" inline-block w-20 h-20 md:w-28 md:h-28 cursor-pointer hover:-translate-y-2 transition-all ease-linear hover:scale-110"
-				/>
+				<div>
+					{language === "C#" ? (
+						<Image
+							key={index}
+							src={`/logos/CSharp.svg`}
+							className=" inline-block w-20 h-20 md:w-28 md:h-28 cursor-pointer hover:-translate-y-2 transition-all ease-linear hover:scale-110"
+						/>
+					) : (
+						<Image
+							key={index}
+							src={`/logos/${language}.svg`}
+							className=" inline-block w-20 h-20 md:w-28 md:h-28 cursor-pointer hover:-translate-y-2 transition-all ease-linear hover:scale-110"
+						/>
+					)}
+				</div>
 			))}
 		</div>
 	);
